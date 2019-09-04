082b956bea6aaff9a4848e82c0881ad4be4a9a27
Soumya Goyal <somyagoyal739@gmail.com>
2019-09-04 23:32:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -2,9 +2,19 @@
 def hopper
 	programmer_hash = 
  		{
-      :known_for => "COBOL",
-      :languages => ["COBOL", "FORTRAN"]
-    }
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
 
 
 end
